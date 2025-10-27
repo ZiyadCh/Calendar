920098347d749a6302311d614789b2cb66af6ac2
ZiyadCh <chetoziyad@gmail.com>
2025-10-27 17:05:32 +0100

    fade

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const data = document.getElementsByTagName("td");
+const modal = document.querySelector(".popclose");
+const fade = document.querySelector(".fadeclose");
+const bdy = document.body;
+
+for (let index = 0; index < data.length; index++) {
+  data[index].addEventListener("click", () => {
+    modal.className = "popopen";
+    fade.className = "fade";
+  });
+}
