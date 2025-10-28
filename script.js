d45c6ae2cb820c8f2d753b00cfa1b7170017622a
ZiyadCh <chetoziyad@gmail.com>
2025-10-28 11:16:26 +0100

    going somewhere

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ const data = document.getElementsByTagName("td");
 const modal = document.querySelector(".popclose");
 const fade = document.querySelector(".fadeclose");
 const bdy = document.body;
+const reTexte = document.getElementById("reTexte");
+const btn = document.getElementById("btn");
 
 for (let index = 0; index < data.length; index++) {
   data[index].addEventListener("click", () => {
     modal.className = "popopen";
     fade.className = "fade";
-    data[index].innerHTML = `${index + 1} <h1>yes</h1>`;
-    reserve.innerHTML = "yes";
+
+    btn.addEventListener("click", () => {
+      data[index].innerHTML = `${index + 1} <h1>${reTexte.value}</h1>`;
+    });
   });
 }
+
+btn.addEventListener("click", () => {
+  data[index].innerHTML = `${index + 1} <h1>${reTexte.value}</h1>`;
+});
 
 fade.addEventListener("click", () => {
   modal.className = "popclose";
   fade.className = "fadeclose";
 });
+
+function proceed(index) {
+  data[index].innerHTML = `${index + 1} <h1>${reTexte.value}</h1>`;
+}
