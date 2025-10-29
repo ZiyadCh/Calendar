f12195495420bcebba3aa5daa6b660d8d2f2f889
ZiyadCh <chetoziyad@gmail.com>
2025-10-29 10:10:29 +0100

    g

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const data = document.getElementsByTagName("td");
 const modal = document.querySelector(".popclose");
 const fade = document.querySelector(".fadeclose");
-const bdy = document.body;
 const reTexte = document.getElementById("reTexte");
 const btn = document.getElementById("btn");
+const time_start = document.getElementById("time_start");
 let target;
 
 for (let index = 0; index < data.length; index++) {
@@ -15,7 +15,10 @@ for (let index = 0; index < data.length; index++) {
 }
 btn.addEventListener("click", () => {
   target.classList.add("reservation");
-  target.innerHTML = `${target.innerHTML} <div class="card"><h3>${reTexte.value}</h3> </div>`;
+  target.innerHTML = `${target.innerHTML} <div class="card"> <p>${time_start.value}</p> <p>${reTexte.value}</p> </div>`;
+
+  modal.className = "popclose";
+  fade.className = "fadeclose";
 });
 fade.addEventListener("click", () => {
   modal.className = "popclose";
